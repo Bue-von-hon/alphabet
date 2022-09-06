25ec1af05ab7c82a6fae9f066bd4fb9cc3dfe940
Bue-von-hon <dkssudvn2@gmail.com>
2022-09-06 23:39:01 +0900

    fileReader를 이용해 뱃지를 제작하는 메소드 추가
    메소드 호출마다 파일을 읽지 않고 클래스 변수에 저장하고 재활용

--- src/main/java/uhs/alphabet/domain/badge/StuBadge.java
+++ src/main/java/uhs/alphabet/domain/badge/StuBadge.java
@@ -3,11 +3,34 @@ package uhs.alphabet.domain.badge;
 import org.springframework.core.io.ClassPathResource;
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 
 public class StuBadge {
     private static final Charset charset = Charset.forName("UTF-8");
-    public static String of(final String name, final String handle) {
+    private static final String data;
+    static {
+        ClassPathResource classPathResource = new ClassPathResource("/static/badge/stuBadge");
+        String ret = "";
+        try {
+            File file = classPathResource.getFile();
+            FileReader fileReader = new FileReader(file);
+            CharBuffer charBuffer = CharBuffer.allocate((int) file.length());
+            int read = fileReader.read(charBuffer);
+            charBuffer.flip();
+            ret = charBuffer.toString();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        data = ret;
+    }
+    public static String of2(final String name, final String handle) {
+        return data
+                .replaceAll("\\{(name)}", name)
+                .replaceAll("\\{(handle)}", handle);
+    }
+    public static String of1(final String name, final String handle) {
        String badge = "";
        ClassPathResource classPathResource = new ClassPathResource("/static/badge/stuBadge");
        try (InputStream in = classPathResource.getInputStream()) {
@@ -18,7 +41,7 @@ public class StuBadge {
            badge = badge.replaceAll("\\{(name)}", name).replaceAll("\\{(handle)}", handle);
        }
        catch (Exception e) {
-           System.out.println(e.toString()+" "+e.getMessage());
+           System.out.println(e.getMessage());
        }
        return badge;
     }
