77ba6bdb421a265b0794722750f3edb5abc853e2
Bue-von-hon <dkssudvn2@gmail.com>
2022-09-15 08:16:27 +0900

    뱃지 api 캐시 적용
    외부 api와 연동된 부분에 캐시 적용

---
+++ src/main/java/uhs/alphabet/config/CacheConfig.java
@@ -0,0 +1,28 @@
+package uhs.alphabet.config;
+
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uhs.alphabet.domain.badge.CfUser;
+
+import java.time.Duration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+    @Bean
+    public CacheManager ehCacheCacheManager() {
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .withCache("codeforcesCache",
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, CfUser.class, ResourcePoolsBuilder.heap(10))
+                                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofDays(1))))
+                .build();
+        cacheManager.init();
+        return cacheManager;
+    }
+}
--- src/main/java/uhs/alphabet/domain/badge/BadgeController.java
+++ src/main/java/uhs/alphabet/domain/badge/BadgeController.java
@@ -1,6 +1,8 @@
 package uhs.alphabet.domain.badge;
 
 import lombok.RequiredArgsConstructor;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BadgeController {
     private final PersonService personService;
+    private final CacheManager cacheManager;
 
     @GetMapping(value = "/stubadge")
-    public String a(@RequestParam("stuid") String stuid) {
+    public String stubadge(@RequestParam("stuid") String stuid) {
         List<PersonDto> personDtos = personService.searchPerson(stuid);
         String handle = "None";
         String name = "None";
@@ -26,8 +29,13 @@ public class BadgeController {
     }
 
     @GetMapping("/cfbadge")
-    public String b(@RequestParam("handle") String handle) {
-        CfUser cfUser = CfUser.of(handle);
+    public String cfbadge(@RequestParam("handle") String handle) {
+        Cache<String, CfUser> codeforcesCache = cacheManager.getCache("codeforcesCache", String.class, CfUser.class);
+        CfUser cfUser = codeforcesCache.get(handle);
+        if (cfUser == null) {
+            cfUser = CfUser.of(handle);
+            codeforcesCache.put(handle, cfUser);
+        }
         return CfBadge.of(cfUser.getHandle(), cfUser.getColor());
     }
 }
