483199d18aa397ee981bc3714314a89e2b94dfd2
Bue-von-hon <dkssudvn2@gmail.com>
2022-09-21 00:01:44 +0900

    handle 파라미터 검증

--- src/main/java/uhs/alphabet/badge/BadgeController.java
+++ src/main/java/uhs/alphabet/badge/BadgeController.java
@@ -3,12 +3,17 @@ package uhs.alphabet.badge;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class BadgeController {
     private final BadgeService badgeService;
 
@@ -21,7 +26,7 @@ public class BadgeController {
 
     @GetMapping(value = "/cfbadge", produces = "image/svg+xml")
     @Cacheable( cacheNames = "codeforcesCache")
-    public String getCodeforcesBadge(@RequestParam("handle") String handle) {
+    public String getCodeforcesBadge(@RequestParam("handle") @Valid @Size(min = 2, max = 24) String handle) {
         String badge = badgeService.makeCodeforcesBadge(handle);
         return badge;
     }
