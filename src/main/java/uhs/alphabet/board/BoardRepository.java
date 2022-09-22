c9a19dfbf921fde3169e79d14101e0a873733eba
Bue-von-hon <dkssudvn2@gmail.com>
2022-09-22 17:14:22 +0900

    polishing

--- src/main/java/uhs/alphabet/board/BoardRepository.java
+++ src/main/java/uhs/alphabet/board/BoardRepository.java
@@ -1,10 +1,8 @@
 package uhs.alphabet.board;
 
-import uhs.alphabet.board.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     List<BoardEntity> findByTitleContaining(String key);
