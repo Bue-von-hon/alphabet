da89c26996fa312a7b1f7a1a67a7c18be0410ad6
Bue-von-hon <dkssudvn2@gmail.com>
2022-09-29 11:26:28 +0900

    Badgeservice에서 BadgeMaster 이용하도록 수정

--- src/main/java/uhs/alphabet/badge/BadgeService.java
+++ src/main/java/uhs/alphabet/badge/BadgeService.java
@@ -3,9 +3,7 @@ package uhs.alphabet.badge;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
-import uhs.alphabet.badge.codeforces.CodeForcesBadge;
-import uhs.alphabet.badge.codeforces.util.CodeforcesHttpClient;
-import uhs.alphabet.badge.codeforces.util.CodeforcesMapper;
+import uhs.alphabet.badge.codeforces.CodeforcesBadgeMaster;
 import uhs.alphabet.badge.students.StudentBadgeMaster;
 import uhs.alphabet.domain.entity.PersonEntity;
 import uhs.alphabet.domain.repository.PersonRepository;
@@ -14,16 +12,13 @@ import uhs.alphabet.domain.repository.PersonRepository;
 @RequiredArgsConstructor
 public class BadgeService {
     private final PersonRepository personRepository;
-    private final CodeforcesHttpClient codeforcesClient;
-    private final CodeforcesMapper codeforcesMapper;
-
     public String getStudentBadgeById(String stuid) {
         PersonEntity personEntity = personRepository.findByStunum(stuid);
         return StudentBadgeMaster.getBadgeByEntity(personEntity);
     }
 
     public String  makeCodeforcesBadge(final String handle) {
-        CodeForcesBadge badge = new CodeForcesBadge(handle, codeforcesMapper, codeforcesClient);
-        return badge.getBadge();
+        CodeforcesBadgeMaster codeforcesBadgeMaster = new CodeforcesBadgeMaster();
+        return codeforcesBadgeMaster.getBadgeByHandle(handle);
     }
 }
