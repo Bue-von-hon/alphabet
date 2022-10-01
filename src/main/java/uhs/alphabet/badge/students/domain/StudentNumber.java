f081c51e18a91377f4fbd2984ba7eeaefbaa7ca3
Bue-von-hon <dkssudvn2@gmail.com>
2022-10-01 15:53:51 +0900

    number-format Exception 추가

--- src/main/java/uhs/alphabet/badge/students/domain/StudentNumber.java
+++ src/main/java/uhs/alphabet/badge/students/domain/StudentNumber.java
@@ -3,10 +3,15 @@ package uhs.alphabet.badge.students.domain;
 public class StudentNumber {
     private final String number;
 
-    public StudentNumber(String number) {
+    private StudentNumber(String number) {
+        this.number = number;
+    }
+
+    public static StudentNumber createByString(String number) {
         if (number == null) throw new RuntimeException("학번은 null이 안됩니다.");
         if (number.length() != 8) throw new RuntimeException("학번은 8자리입니다.");
-        this.number = number;
+        Integer.parseInt(number);
+        return new StudentNumber(number);
     }
 
     public String getNumber() {
--- src/test/java/uhs/alphabet/badge/domain/StudentNumberTest.java
+++ src/test/java/uhs/alphabet/badge/domain/StudentNumberTest.java
@@ -10,14 +10,14 @@ public class StudentNumberTest {
     @Test
     @DisplayName("8자리 학번 생성 테스트")
     public void test1() {
-        Assertions.assertDoesNotThrow(() -> new StudentNumber("20221122"));
+        Assertions.assertDoesNotThrow(() -> StudentNumber.createByString("20221122"));
     }
 
     @Test
     @DisplayName("6자리 학번 생성 테스트")
     public void test2() {
         RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> {
-            new StudentNumber("123456");
+            StudentNumber.createByString("123456");
         });
         Assertions.assertTrue(runtimeException.getMessage().contains("학번은 8자리입니다."));
     }
@@ -26,8 +26,16 @@ public class StudentNumberTest {
     @DisplayName("null 학번 생성 테스트")
     public void test3() {
         RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> {
-            new StudentNumber(null);
+            StudentNumber.createByString(null);
         });
         Assertions.assertTrue(runtimeException.getMessage().contains("학번은 null이 안됩니다."));
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 학번 생성 테스트")
+    public void test4() {
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> {
+            StudentNumber.createByString("test1234");
+        });
     }
 }
